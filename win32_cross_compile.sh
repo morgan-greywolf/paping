e346bf33a0271a1ac28eafade81a19cd27846dfc
Rob Shinn <rshinn@e-one.com>
2020-01-20 14:07:10 -0500

    Latest Updates

--- win32_cross_compile.sh
+++ win32_cross_compile.sh
@@ -2,4 +2,4 @@
 # To cross-compile for Win32 on *nix, you need to have some sort of mingw
 # cross-compiler installed and point to it through the CXX variable.
 # Additionally, you need to pass the -DWIN32 to it.
-CXX=i586-mingw32msvc-g++ CCFLAGS="-DWIN32" scons $*
+CXX=i586-mingw32msvc-g++ CCFLAGS="-DWIN32 -fpermissive" scons $*
